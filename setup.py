06a7dc14a5cb9d7f9677ae13012cf3ad9ca249c2
indaco biazzo <ocadni@users.noreply.github.com>
2022-06-09 13:02:41 +0200

    fix tests

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name="bpepi",
     version="0.1dev",
-    packages=["bpepi", "numpy", "networkx"],
-    install_requires=["numpy"],  # external packages as dependencies
+    packages=["bpepi"],
+    install_requires=["numpy", "networkx"],  # external packages as dependencies
     license="Apache License Version 2.0,",
     long_description=open("README.md").read(),
 )
