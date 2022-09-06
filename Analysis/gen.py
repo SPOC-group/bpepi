3a7c19de45b71459d38fbd96c1af9c47d256f6a0
DavideGhioEPFL <davide.ghio@epfl.ch>
2022-09-06 10:23:33 +0200

    Fixed dSIR sim

--- Analysis/gen.py
+++ Analysis/gen.py
@@ -52,7 +52,7 @@ def simulate_one_detSIR(G, s_type = "delta", S = 0.01, mask = ["SI"], T_max=100)
     status_nodes.append(np.array(s0))
     # Generate the epidemics
     st = np.copy(s0)
-    while ((flag_m==1 and 1 in status_nodes[-1]) or ( (flag_m==0 or flag_sir==1) and 0 in status_nodes[-1])) and (len(status_nodes) <= T_max):
+    while ((flag_m==1 and 1 in status_nodes[-1]) or ( flag_m==0 and 0 in status_nodes[-1]) or (flag_sir==1 and (0 in status_nodes[-1]) and (len(status_nodes) < T_max))) and (len(status_nodes) <= T_max):
         st_minus_1 = np.copy(st)
         coeff_minus_1 = coeff_lam.copy()
         for i in range(N):
