be224fc0662740c0aafe0f1a4375132d8687c536
indaco biazzo <ocadni@users.noreply.github.com>
2022-11-29 12:18:02 +0100

    add pytorch to script

--- Analysis/run_oc_SI_snap2.sh
+++ Analysis/run_oc_SI_snap2.sh
@@ -27,5 +27,6 @@ damping=0.2
 #is=10
 n_iter=500
 it_max=50000
+pytorch=0 # Using pytorch backend
 
-$python $script --damping $damping --n_iter $n_iter --it_max $it_max --snap_time $snap_time --save_dir $save_dir --save_DF_dir $save_DF_dir --graph $graph --N $N --d $d --lam $lam --delta $delta --rho $rho --nsim $nsim --seed $seed --snap --SI --rnd_inf_init
+$python $script --damping $damping --n_iter $n_iter --it_max $it_max --snap_time $snap_time --save_dir $save_dir --save_DF_dir $save_DF_dir --graph $graph --N $N --d $d --lam $lam --delta $delta --rho $rho --nsim $nsim --seed $seed --pytorch $pytorch --snap --SI --rnd_inf_init 
--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from distutils.core import setup
 setup(
     name="bpepi",
     version="0.1dev",
-    packages=["bpepi","bpepi.Modules"],
-    install_requires=["numpy", "networkx"],  # external packages as dependencies
+    packages=["bpepi", "bpepi.Modules"],
+    install_requires=[
+        "numpy",
+        "networkx",
+        "pytorch",
+    ],  # external packages as dependencies
     license="Apache License Version 2.0,",
     long_description=open("README.md").read(),
 )
