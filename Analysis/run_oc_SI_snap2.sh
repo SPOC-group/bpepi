2bdda2fcc8654023b3675bd725648f6772df6542
DavideGhioEPFL <davide.ghio@epfl.ch>
2022-11-29 13:21:26 +0100

    merge

--- Analysis/run_oc_SI_snap2.sh
+++ Analysis/run_oc_SI_snap2.sh
@@ -11,8 +11,8 @@
 
 python="/home/ghio/miniconda3/envs/sib/bin/python"
 script="/home/ghio/bpepi/Analysis/sim_oc_dSIR.py"
-save_dir="./data/SI_Dsnap_delta_ave/"
-save_DF_dir="./data_frames/SI_Dsnap_delta_ave/"
+save_dir="./data/SI_Dsnap_delta_lam/"
+save_DF_dir="./data_frames/SI_Dsnap_delta_lam/"
 graph="rrg"
 N=10000
 d=3
@@ -22,10 +22,10 @@ delta=0.02
 rho=1.
 nsim=1
 seed=33
-snap_time=50
+snap_time=9
 damping=0.2
 #is=10
-n_iter=500
-it_max=50000
+n_iter=200
+it_max=20000
 
 $python $script --save_marginals --damping $damping --n_iter $n_iter --it_max $it_max --snap_time $snap_time --save_dir $save_dir --save_DF_dir $save_DF_dir --graph $graph --N $N --d $d --lam $lam --delta $delta --rho $rho --nsim $nsim --seed $seed --snap --SI --rnd_inf_init
