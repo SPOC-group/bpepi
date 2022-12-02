f54fd64f43bb62b35baa3480d6b764f248686fa5
indaco biazzo <ocadni@gmail.com>
2022-12-02 11:08:38 +0100

    pytorch script

--- Analysis/run_oc_SI_snap_torch.sh
+++ Analysis/run_oc_SI_snap_torch.sh
@@ -3,30 +3,30 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --output=run.log
-#SBATCH --mem=8GB
+#SBATCH --mem=4GB
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=indaco.biazzo@epfl.ch
 
 python="/home/biazzo/miniconda3/envs/sib/bin/python"
 script="/home/biazzo/git/bpepi/Analysis/sim_oc_dSIR.py"
-save_dir="./data/snap_indaco/"
-save_DF_dir="./data_frames/snap_df_indaco/"
+save_dir="./data/snap_indaco/npippo/"
+save_DF_dir="./data_frames/snap_df_indaco/npippo/"
 graph="rrg"
-N=10
+N=2000
 d=3
 lam=0.6
 #n_sources=1
-delta=0.1
+delta=0.09
 rho=1.
 nsim=1
-seed=33
-snap_time=7.3
+seed=1
+snap_time=1
 damping=0.2
 #is=10
-n_iter=500
-it_max=500
-pytorch=0 # Using pytorch backend
+n_iter=200
+it_max=20000
+pytorch=1 # Using pytorch backend
 
 $python $script --save_marginals --damping $damping --n_iter $n_iter --it_max $it_max --snap_time $snap_time --save_dir $save_dir --save_DF_dir $save_DF_dir --graph $graph --N $N --d $d --lam $lam --delta $delta --rho $rho --nsim $nsim --pytorch $pytorch --seed $seed --snap --SI --rnd_inf_init 
