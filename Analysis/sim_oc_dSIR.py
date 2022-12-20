be7d627821161c3d60e2f5d5afff8c1a5d92dd16
DavideGhioEPFL <davide.ghio@epfl.ch>
2022-12-20 18:23:35 +0100

    Fix?

--- Analysis/sim_oc_dSIR.py
+++ Analysis/sim_oc_dSIR.py
@@ -1,3 +1,7 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
 import numpy as np
 import networkx as nx
 import random
